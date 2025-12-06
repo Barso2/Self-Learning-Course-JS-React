8f1d47d55183b81908f9cecf12ff71535c7d68e5
Alex <reznitskyalex@gmail.com>
2025-12-06 18:55:30 +0200

    lesson 018 task completed, using functions

--- script.js
+++ script.js
@@ -1,4 +1,17 @@
-const numberOfFilms = +prompt('how many movies did you see?', '');
+'use strict';
+
+let numberOfFilms;
+
+function start() {
+    numberOfFilms = +prompt('How many movies did you see?', '');
+    while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
+        alert('You must enter a number bigger that 0')
+        numberOfFilms = +prompt('How many movies did you see?', '');
+    }
+
+}
+start();
+
 
 const personlaMovieDB = {
     count: numberOfFilms,
@@ -8,12 +21,46 @@ const personlaMovieDB = {
     privat: false
 };
 
-const a = prompt('one of last seen movies?', ''),
-    b = prompt('rating of current movie', ''),
-    c = prompt('one of last seen movies?', ''),
-    d = prompt('rating of current movie', '');
 
-personlaMovieDB.movies[a] = b;
-personlaMovieDB.movies[c] = d;
+function rememberMyFilms() {
+    for (let i = 0; i < personlaMovieDB.count; i++) {
+        const lastMovieName = prompt('One of last seen movies?', ''),
+            currentMovieRating = +prompt('Rating of current movie (1 - 10)', '')
+        if (lastMovieName && lastMovieName.length > 0 && lastMovieName.length <= 50 && currentMovieRating) {
+            personlaMovieDB.movies[lastMovieName] = currentMovieRating;
+        } else {
+            if (lastMovieName.length > 50) {
+                alert('Movie name lenght must be less than 50 characters!')
+            } else if (!lastMovieName) {
+                alert('You must enter movie name');
+            } else if (!currentMovieRating || isNaN(currentMovieRating)) {
+                alert('You must rate the movie (1 - 10)');
+            }
+            i--
+        };
+    }
+}
 
-console.log(personlaMovieDB);
+function detectPersonalLevel() {
+    if (personlaMovieDB.count > 0 && personlaMovieDB.count <= 30) {
+        alert('Youa are movie lover')
+    } else if (personlaMovieDB.count > 30) {
+        alert('You are movie freak!')
+    } else { alert("Error occured") }
+}
+
+function showMyDB(hiden) {
+    if (!hiden) {
+        console.log(personlaMovieDB);
+    }
+}
+
+function writeYourGenres() {
+    for (let i = 1; i <= 3; i++) {
+        personlaMovieDB.genres.push(prompt(`Your favorite genre at number ${i} is:`));
+    }
+}
+
+writeYourGenres();
+
+showMyDB(personlaMovieDB.privat);
