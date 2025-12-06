7fbc2a3ee894957de6af034571eb9aa566382d13
Alex <reznitskyalex@gmail.com>
2025-12-06 15:06:57 +0200

    start of movies app

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
-logger();
+const numberOfFilms = +prompt('how many movies did you see?', '');
 
-function logger() {
-    console.log("logger function");
+const personlaMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
 };
+
+const a = prompt('one of last seen movies?', ''),
+    b = prompt('rating of current movie', ''),
+    c = prompt('one of last seen movies?', ''),
+    d = prompt('rating of current movie', '');
+
+personlaMovieDB.movies[a] = b;
+personlaMovieDB.movies[c] = d;
+
+console.log(personlaMovieDB);
